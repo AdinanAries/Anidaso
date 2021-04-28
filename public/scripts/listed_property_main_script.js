66cc115f288de070c25d156f5bf8fa6352fce674
Mohammed Adinan <adinanaries@outlook.com>
2021-04-28 00:18:34 -0400

    added room availability grid view for hotel managers dashboard

--- public/scripts/listed_property_main_script.js
+++ public/scripts/listed_property_main_script.js
@@ -2402,9 +2402,9 @@ $(function() {
   
       //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
-  });
+});
 
-  $(function() {
+$(function() {
     $('#make_reservation_date_range_input').daterangepicker({
       opens: 'left',
       locale: {
@@ -2423,7 +2423,49 @@ $(function() {
   
       //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
-  });
+});
+
+$(function() {
+    $('#make_reservation_date_range_on_popup_input').daterangepicker({
+      opens: 'left',
+      locale: {
+        cancelLabel: 'Clear'
+      }
+    }, function(start, end, label) {
+  
+      setTimeout(()=>{
+        document.getElementById("make_reservation_date_range_on_popup_input").value = start.toString().substring(0,11) +"  -  "+ end.toString().substring(0,11);
+      }, 100);
+  
+      //fligh_search_data.departure_date = start.format('YYYY-MM-DD');
+      //fligh_search_data.return_date = end.format('YYYY-MM-DD');
+  
+      //window.localStorage.setItem("flights_post_data", JSON.stringify(fligh_search_data));
+  
+      //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
+
+$(function() {
+    $('#make_reservation_date_range_on_popup_chekin_checkout_input').daterangepicker({
+      opens: 'left',
+      locale: {
+        cancelLabel: 'Clear'
+      }
+    }, function(start, end, label) {
+  
+      setTimeout(()=>{
+        document.getElementById("make_reservation_date_range_on_popup_chekin_checkout_input").value = start.toString().substring(0,11) +" - "+ end.toString().substring(0,11);
+      }, 100);
+  
+      //fligh_search_data.departure_date = start.format('YYYY-MM-DD');
+      //fligh_search_data.return_date = end.format('YYYY-MM-DD');
+  
+      //window.localStorage.setItem("flights_post_data", JSON.stringify(fligh_search_data));
+  
+      //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
 
 function get_logged_in_hotel_all_photos(hotel_id){
     return $.ajax({
---
+++ public/scripts/rooms_availability_grid_view_script.js
@@ -0,0 +1,14 @@
+var rooms_availability = [
+    {
+        room_id: "",
+        room_number: "",
+        days: [
+            {
+                date: 21,
+                classes: [
+                    "css class"
+                ]
+            }
+        ]
+    }
+]
